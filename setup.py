b93051e4d7fd6319721f96fbf1799d592c67e976
singingwithcode <klich.mw@pg.com>
2023-01-05 17:17:35 -0600

    version update

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ classifiers = [
 
 setup(
     name="YAML2ST",
-    version="1.0.18",
+    version="1.0.19",
     description="YAML2ST automates streamlit input widgets from a YAML.",
     long_description=long_description,
     long_description_content_type="text/markdown",
